69e4176092ab8fa6c8ae2c1058abb5930d2bd76a
jnicolasdz <jndiazs@udistrital.edu.co>
2026-05-18 14:43:19 -0500

    uploading project structure

---
+++ app/main.py
@@ -0,0 +1,7 @@
+"""Initializes the FastApi application, including:
+        -creating the app instance
+        -registering the API routers
+        -attaching the database session to the app state (middleware)
+        -loading environment variables from a .env file (aplication settings)
+        -configuring CORS settings for cross-origin requests
+"""
