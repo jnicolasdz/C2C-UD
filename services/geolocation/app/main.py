c18404e7a6ff115c6ef8c3c67d07c85a281a9981
jnicolasdz <jndiazs@udistrital.edu.co>
2026-05-31 18:01:11 -0500

    fix(main): moving cors import to able its import by the correct path

--- services/geolocation/app/main.py
+++ services/geolocation/app/main.py
@@ -1,7 +1,5 @@
 import sys
 from pathlib import Path
-from app.core.cors import add_cors_middleware
-
 from fastapi import FastAPI
 
 # Allow running this file directly: python ./app/main.py
@@ -9,6 +7,7 @@ if __package__ is None or __package__ == "":
 	sys.path.append(str(Path(__file__).resolve().parent.parent))
 
 from app.api.v1.routes import router
+from app.core.cors import add_cors_middleware
 
 app = FastAPI(title="Geolocation Service")
 add_cors_middleware(app)
