d6ca29e05231fdf24da6fde316956a87e0a2f5bb
jnicolasdz <jndiazs@udistrital.edu.co>
2026-06-02 09:34:18 -0500

    feat(Dockerfile): adding running test on the docker

--- services/coupon/app/main.py
+++ services/coupon/app/main.py
@@ -1,9 +1,7 @@
 import sys
 from pathlib import Path
 from fastapi import FastAPI
-import uvicorn
 
-# Allow running this file directly: python ./app/main.py
 if __package__ is None or __package__ == "":
 	sys.path.append(str(Path(__file__).resolve().parent.parent))
 
@@ -12,7 +10,5 @@ from app.api.v1.routers import router
 app = FastAPI(title="Cuopon Service")
 app.include_router(router, prefix="/api/v1")
 
-if __name__ == "__main__":
-	uvicorn.run(app, host="127.0.0.1", port=8000)
 
 
